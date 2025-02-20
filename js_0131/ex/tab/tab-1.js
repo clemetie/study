1c56b8da67aaf8a988b721c60025fcc21806e141
hyobeen <a01035083619@gmail.com>
2025-02-20 14:24:25 +0900

    no message

--- js_0131/ex/tab/tab-1.js
+++ js_0131/ex/tab/tab-1.js
@@ -1,3 +1,36 @@
-window.addEventListener("load", ()=>{
-    
-})
+window.addEventListener("load", () => {
+  const popupBtnLinks = document.querySelectorAll(".popup-btn .inner a");
+  const popupBtn = document.querySelector(".popup-btn");
+  const popupTxt = document.querySelector(".pop-text");
+  popupBtnLinks.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      popupBtnLinks.forEach((link) => {
+        link.classList.toggle("active");
+      });
+    });
+  });
+  popupBtn.addEventListener("click", () => {
+    popupTxt.style.display = popupTxt.style.display === "none" ? "block" : "none";
+  });
+  // 메뉴 탭 기능
+  const tabTitle = document.querySelectorAll(".tab-nav li a");
+  tabTitle.forEach(function (element) {
+    element.addEventListener("click", function (e) {
+      e.preventDefault();
+      tabTitle.forEach(function (item) {
+        item.classList.remove("active");
+      });
+      this.classList.add("active");
+
+      const tabItems = document.querySelectorAll(".tabitem");
+      tabItems.forEach(function (tab) {
+        tab.style.display = "none";
+      });
+
+      const target = element.getAttribute("href");
+      document.querySelector(target).style.display = "block";
+    });
+  });
+  tabTitle[0].click();
+});
