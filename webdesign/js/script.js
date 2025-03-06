9677bf5e0cefd9f8f87f6c0a6787c03d87b8d878
hyobeen <a01035083619@gmail.com>
2025-03-06 15:30:01 +0900

    웹디 f-3 완성

--- webdesign/js/script.js
+++ webdesign/js/script.js
@@ -6,11 +6,25 @@ $(function () {
   });
   //   슬라이드
   const slideList = $(".slide-list");
-  const slideWidth = $(".slide");
+  const slideWidth = $(".slide li").width();
   const moveSlide = () => {
     slideList.animate({ marginLeft: -slideWidth }, 1000, function () {
       $(this).append($(this).find("li:first")).css({ marginLeft: 0 });
     });
   };
   setInterval(moveSlide, 3000);
+
+  //탭메뉴
+  $(".tab>li>a").click(function (e) {
+    e.preventDefault();
+    $(this).parent().addClass("active").siblings().removeClass("active");
+  });
+
+  //모달
+  $(".notice li:first-child").click(() => {
+    $("#modal").addClass("active");
+  });
+  $(".modal-close").click(() => {
+    $("#modal").removeClass("active");
+  });
 });
